08080021a689682ea4e800b52345372dcdb1f839
Lucas Fiorelli <fiorellilucas@gmail.com>
2024-08-07 21:50:01 -0300

    Criar navbar

--- corenotes/app/components/navbar.tsx
+++ corenotes/app/components/navbar.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image"
+
 export default function Navbar() {
   return (
-    <h1>navbar</h1>
+    <div className="navbar">
+      <div className="flex items-center">
+        <Image
+          src="/logo.png"
+          width={50}
+          height={50}
+          alt="Logo CoreNotes"
+          className="ml-5"
+        />
+        <p className="hidden sm:block">CoreNotes</p>
+      </div>
+      <input placeholder="Procure uma nota" className="border border-slate-300 rounded-lg px-1 sm:w-1/2 max-w-screen-sm"></input>
+      <button className="sm:px-16 px-6">X</button>
+    </div>
   )
 }
