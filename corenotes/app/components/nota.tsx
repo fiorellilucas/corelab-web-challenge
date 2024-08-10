091554cbb69693d22ec266fc30adc76f37e63cf7
Lucas Fiorelli <fiorellilucas@gmail.com>
2024-08-10 11:50:26 -0300

    Adicionar funcionalidade ao botão editar nota

--- corenotes/app/components/nota.tsx
+++ corenotes/app/components/nota.tsx
@@ -23,11 +23,13 @@ export default function Nota(props: NotaProps) {
     )
     location.reload()
   }
-  
+
   return (
-    <div className={"bg-[#" + props.cor + "] flex flex-col border border-slate-300 rounded-3xl sm:w-1/2 max-w-screen-sm drop-shadow h-72 p-1 px-4 my-14"}>
+    <div className={"bg-[#" + props.cor + "] flex flex-col border border-slate-300 rounded-3xl sm:w-1/2 max-w-screen-sm drop-shadow h-72 p-1 px-4 my-14"} id={"nota" + props.id}>
       <div className="flex justify-between border-b border-black items-center">
-        <h1 className="flex h-auto w-full"><strong>{props.titulo}</strong></h1>
+        <strong>
+          <textarea disabled rows={1} className="flex h-auto w-full bg-inherit py-2" id={"titulo" + props.id} defaultValue={props.titulo} />
+        </strong>
         <Image
           src={props.favorito == true ? "/estrela-amarela.png" : "/estrela-branca.png"}
           width={20}
@@ -37,10 +39,50 @@ export default function Nota(props: NotaProps) {
         />
       </div>
       <div className="flex flex-col grow justify-between">
-        <p className="py-1">{props.conteudo}</p>
+
+        <textarea disabled rows={7} className="py-2 bg-inherit" id={"conteudo" + props.id} defaultValue={props.conteudo} />
+
         <PaletaCores notaId={props.id} />
         <div className="flex justify-between">
-          <button onClick={() => { }}><Image src="/icone-editar.png" width={20} height={20} alt="Editar" /></button>
+          <div>
+            <button onClick={() => {
+              const tituloNota: any = document.getElementById("titulo" + props.id)
+              const conteudoNota: any = document.getElementById("conteudo" + props.id)
+              const iconeConcluir: any = document.getElementById("iconeConcluir" + props.id)
+
+              tituloNota.toggleAttribute("disabled")
+              conteudoNota.toggleAttribute("disabled")
+
+              if (iconeConcluir.classList[0] == "hidden") {
+                iconeConcluir.classList.replace("hidden", "block")
+              } else {
+                iconeConcluir.classList.replace("block", "hidden")
+              }
+            }}>
+              <Image src="/icone-editar.png" width={20} height={20} alt="Editar" id={"iconeEditar" + props.id} />
+            </button>
+
+            <button onClick={() => {
+              const tituloNota: any = document.getElementById("titulo" + props.id)
+              const conteudoNota: any = document.getElementById("conteudo" + props.id)
+
+              fetch("http://localhost:3000/editar?notaId=" + props.id, 
+                {
+                  method: "PUT",
+                  headers: { "Content-Type": "application/json" },
+                  body: JSON.stringify(
+                    {
+                      "titulo": tituloNota.value,
+                      "conteudo": conteudoNota.value
+                    }
+                  )
+                }
+              )
+              location.reload()
+            }}>
+              <Image className="hidden" src="/icone-concluir.png" width={20} height={20} alt="Concluir" id={"iconeConcluir" + props.id} />
+            </button>
+          </div>
 
           <button onClick={() => {
             const paleta: any = document.getElementById("paleta" + props.id)
