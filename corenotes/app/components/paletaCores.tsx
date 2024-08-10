5bb66952d492b4de71b6b7d0abce7298d6252792
Lucas Fiorelli <fiorellilucas@gmail.com>
2024-08-10 08:49:36 -0300

    Adicionar branco à paleta de cores

--- corenotes/app/components/paletaCores.tsx
+++ corenotes/app/components/paletaCores.tsx
@@ -16,6 +16,7 @@ export default function PaletaCores( props: PaletaProps ) {
   
   return (
     <div className="hidden flex-wrap justify-evenly border border-slate-300 rounded-3xl shadow-md bg-white max-w-[268px] self-center" id={"paleta" + props.notaId}>
+      <div className="bg-[#FFFFFF] size-10 border-slate-500 border rounded-full m-1" onClick={() => { TrocarCor("FFFFFF") }}></div>
       <div className="bg-[#BAE2FF] size-10 border-slate-400 border rounded-full m-1" onClick={() => { TrocarCor("BAE2FF") }}></div>
       <div className="bg-[#B9FFDD] size-10 border-slate-400 border rounded-full m-1" onClick={() => { TrocarCor("B9FFDD") }}></div>
       <div className="bg-[#FFE8AC] size-10 border-slate-400 border rounded-full m-1" onClick={() => { TrocarCor("FFE8AC") }}></div>
@@ -23,7 +24,6 @@ export default function PaletaCores( props: PaletaProps ) {
       <div className="bg-[#F99494] size-10 border-slate-400 border rounded-full m-1" onClick={() => { TrocarCor("F99494") }}></div>
       <div className="bg-[#ECA1FF] size-10 border-slate-400 border rounded-full m-1" onClick={() => { TrocarCor("ECA1FF") }}></div>
       <div className="bg-[#DAFF8B] size-10 border-slate-400 border rounded-full m-1" onClick={() => { TrocarCor("DAFF8B") }}></div>
-      <div className="bg-[#979797] size-10 border-slate-400 border rounded-full m-1" onClick={() => { TrocarCor("979797") }}></div>
       <div className="bg-[#CDCDCD] size-10 border-slate-400 border rounded-full m-1" onClick={() => { TrocarCor("CDCDCD") }}></div>
       <div className="bg-[#A99A7C] size-10 border-slate-400 border rounded-full m-1" onClick={() => { TrocarCor("A99A7C") }}></div>    
     </div>
